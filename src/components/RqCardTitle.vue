a146d0b33cfa27fe00a8fcf9a9ef660a1a2b38ab
Заикин Николай Игоревич <Zaikin.NI@ahstep.ru>
2020-05-05 20:42:53 +0300

    Перенесен разделитель

--- src/components/RqCardTitle.vue
+++ src/components/RqCardTitle.vue
@@ -3,8 +3,7 @@
         <v-card-text class="pa-0">
                 <p class="text-center pt-4 headline text--primary">{{ title }}</p>
                 <p class="subtitle-1 font-weight-medium mx-8">{{ sub_message }} <v-btn @click="btnToMyreq()" color="green lighten-2 white--text" small><v-icon left dark>mdi-format-list-bulleted</v-icon>Мои заявки</v-btn></p>
-                <hr/>
-            </v-card-text>
+        </v-card-text>
     </v-container>
 </template>
 
