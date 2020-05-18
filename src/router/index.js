45588040f9a7f2242bb5593d9d02ede2f517c7e0
Заикин Николай Игоревич <Zaikin.NI@ahstep.ru>
2020-05-18 19:43:40 +0300

    Правка маршрутов, добавление маршрута для 1С002

--- src/router/index.js
+++ src/router/index.js
@@ -11,59 +11,64 @@ Vue.use(VueRouter)
     component: () => import(/* webpackChunkName: "services" */ '../views/Services.vue')
   },
   {
+    path: '/1c002',
+    name: '1C002',
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/1C002.vue')
+  },
+  {
     path: '/nu001',
     name: 'Nu001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Nu001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Nu001.vue')
   },
   {
     path: '/ns001',
     name: 'Ns001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ns001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ns001.vue')
   },
   {
     path: '/ph001',
     name: 'Ph001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ph001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ph001.vue')
   },
   {
     path: '/ar001',
     name: 'Ar001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ar001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ar001.vue')
   },
   {
     path: '/rb001',
     name: 'Rb001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Rb001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Rb001.vue')
   },
   {
     path: '/sf001',
     name: 'Sf001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Sf001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Sf001.vue')
   },
   {
     path: '/ms001',
     name: 'Ms001',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ms001.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ms001.vue')
   },
   {
     path: '/ms002',
     name: 'Ms002',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ms002.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ms002.vue')
   },
   {
     path: '/Ms003',
     name: 'Ms003',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ms003.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ms003.vue')
   },
   {
     path: '/Ms004',
     name: 'Ms004',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Ms004.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Ms004.vue')
   },
   {
     path: '/Inc',
     name: 'Inc',
-    component: () => import(/* webpackChunkName: "services" */ '../views/Inc.vue')
+    component: () => import(/* webpackChunkName: "services" */ '../views/services/Inc.vue')
   }
 ]
 
