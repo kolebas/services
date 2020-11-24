c4d297a75647e10512a1660c6000bf3c01546e59
Заикин Николай Игоревич <Zaikin.NI@ahstep.ru>
2020-11-24 14:18:29 +0300

    Добавлена информационная иконка

--- src/components/RqCardTitle.vue
+++ src/components/RqCardTitle.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-card-text class="pa-0">
                 <p class="text-center pt-4 headline text--primary">{{ title }}</p>
-                <p class="subtitle-1 font-weight-medium mx-8">{{ sub_message }} <v-btn @click="btnToMyreq()" color="green lighten-2 white--text" small><v-icon left dark>mdi-format-list-bulleted</v-icon>Мои заявки</v-btn></p>
+                <p class="subtitle-1 font-weight-medium mx-8"><v-icon left color="blue darken-2"> mdi-information-outline</v-icon>{{ sub_message }} <v-btn @click="btnToMyreq()" color="green lighten-2 white--text" small><v-icon left dark>mdi-format-list-bulleted</v-icon>Мои заявки</v-btn></p>
         </v-card-text>
     </v-container>
 </template>
