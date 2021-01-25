592ebb9ea3585dc0a3754a8b5dcfd6889b0f0933
Николай <kolebas1986@gmail.com>
2021-01-25 14:35:40 +0300

    Добавлена опции отвключения вывода описания заявки

--- src/components/RqCardTitle.vue
+++ src/components/RqCardTitle.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-card-text class="pa-0">
                 <p class="text-center pt-4 headline text--primary">{{ title }}</p>
-                <p class="subtitle-1 font-weight-medium mx-8"><v-icon left color="blue darken-2"> mdi-information-outline</v-icon>{{ sub_message }} <v-btn @click="btnToMyreq()" color="green lighten-2 white--text" small><v-icon left dark>mdi-format-list-bulleted</v-icon>Мои заявки</v-btn></p>
+                <p v-if="sub_message_enable" class="subtitle-1 font-weight-medium mx-8"><v-icon left color="blue darken-2"> mdi-information-outline</v-icon>{{ sub_message }} <v-btn @click="btnToMyreq()" color="green lighten-2 white--text" small><v-icon left dark>mdi-format-list-bulleted</v-icon>Мои заявки</v-btn></p>
         </v-card-text>
     </v-container>
 </template>
@@ -12,6 +12,9 @@ export default {
     props: {
         title: {
             type: String
+        },
+        sub_message_enable: {
+            type: Boolean, default: true
         },
         sub_message: {
             type: String
