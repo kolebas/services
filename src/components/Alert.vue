594f7fbe5cd81f02310ad776acb8eaec15bd682a
Николай <kolebas1986@gmail.com>
2021-02-19 11:52:06 +0300

    Новый компонент

---
+++ src/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+    <transition>
+      <v-alert prominent type="error">
+        <v-row align="center">
+          <v-col class="grow">
+            {{ text }}
+          </v-col>
+          <v-col class="shrink">
+            <v-btn @click="formCancl()">Назад</v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
+    </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: { type: Boolean, default: false },
+    text: { type: String },
+  },
+  data: () => ({
+  }),
+  methods: {
+    formCancl: function () {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
