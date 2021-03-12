b91322c281770ed31ce3a3cecbe77ea5be1685b3
Николай <kolebas1986@gmail.com>
2021-03-12 11:33:38 +0300

    Добавлены новые props

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container fluid>
     <transition>
-      <v-alert prominent type="error">
+      <v-alert prominent :type="type" :colored-border="colorBorder" :border="borderType" elevation="2">
         <v-row align="center">
           <v-col class="grow">
             {{ text }}
           </v-col>
-          <v-col class="shrink">
+          <v-col v-if="btnBack" class="shrink">
             <v-btn @click="formCancl()">Назад</v-btn>
           </v-col>
         </v-row>
@@ -19,6 +19,11 @@
 export default {
   props: {
     visible: { type: Boolean, default: false },
+    btnBack: { type: Boolean, default: true},
+    type: { type: String, default: "error"},
+    borderType: { type: String, default: "error"},
+    colorBorder: { type: Boolean, default: false},
+    alertElevation: { type: String, default: "2"},
     text: { type: String },
   },
   data: () => ({}),
