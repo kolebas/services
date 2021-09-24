30c80a8bc80891cdd29de5cbd1ba19dfafa34226
Zaikin Nikolay <kolebas1986@gmail.com>
2021-09-24 16:08:55 +0300

    Добавлены комментарии

--- public/ajax/ajax_op002.php
+++ public/ajax/ajax_op002.php
@@ -1,4 +1,6 @@
 <?
+#Отображение ранее созданных заявок в Dashboard
+
 require_once($_SERVER["DOCUMENT_ROOT"].'/bitrix/modules/main/include/prolog_before.php');
     
     if(isset($_GET["getItems"])){
