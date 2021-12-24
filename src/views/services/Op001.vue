ed4c11bbefd969bcfe41166154a6aaf9cb4ee255
Zaikin Nikolay <kolebas1986@gmail.com>
2021-12-24 09:39:29 +0300

    В форму добавлено поле выбора категории

--- src/views/services/Op001.vue
+++ src/views/services/Op001.vue
@@ -3,129 +3,133 @@
     <DialogAfterSendFrom :dialog="dialog" :warnMessage="dialogMessage" />
     <TitleService />
     <v-card min-height="800px" class="py-12">
-    <v-row>
-      <v-card width="65%" raised class="mx-auto" color="grey lighten-4">
-        <RqCardTitle
-          :title="$router.currentRoute.name"
-          :sub_message="sub_message"
-        ></RqCardTitle>
-        <hr />
-        <Input :arrInput="input" />
-        <hr />
-        <v-toolbar flat>
-          <v-toolbar-title>Агрономические параметры</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
-          <div>
-            <v-btn
-              color="primary"
-              elevation="2"
-              :disabled="disableBtn"
-              class="mb-2"
-              small
-              @click="dialogParam = true"
-              >Добавить параметры</v-btn
-            >
-          </div>
-        </v-toolbar>
-        <v-divider></v-divider>
-        <v-dialog v-model="dialogParam" max-width="500px">
-          <v-card>
-            <v-card-title>
-              <span class="headline">Новый параметр</span>
-            </v-card-title>
-            <v-divider />
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="12">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Наименование"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.amount"
-                      label="Количество шт."
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.dv"
-                      label="ДВ"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.phase"
-                      label="Фаза внесения"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="12">
-                    <v-text-field
-                      v-model="editedItem.price"
-                      label="Расход и стоимость л/кг на га"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Отмена </v-btn>
-              <v-btn color="blue darken-1" text @click="save">
-                Сохранить
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="650px">
-          <v-card>
-            <v-card-title class="headline"
-              >Вы действитель хотите удалить данный параметр?</v-card-title
-            >
-            <v-divider />
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Нет</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >Да</v-btn
+      <v-row>
+        <v-card width="65%" raised class="mx-auto" color="grey lighten-4">
+          <RqCardTitle
+            :title="$router.currentRoute.name"
+            :sub_message="sub_message"
+          ></RqCardTitle>
+          <hr />
+          <Input :arrInput="input" />
+          <hr />
+          <v-toolbar flat>
+            <v-toolbar-title>Агрономические параметры</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <div>
+              <v-btn
+                color="primary"
+                elevation="2"
+                :disabled="disableBtn"
+                class="mb-2"
+                small
+                @click="dialogParam = true"
+                >Добавить параметры</v-btn
               >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-data-table
-          :headers="headers"
-          :items="params"
-          class="elevation-1"
-          hide-default-footer
-        >
-          <template v-slot:[`item.actions`]="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-          </template></v-data-table
-        >
-        <v-card-actions class="py-4">
-          <div class="mx-auto">
-            <v-btn
-              class="mx-1"
-              :loading="btnLoader"
-              :disabled="btnStatus"
-              color="green lighten-2 white--text"
-              @click="formSend()"
-            >
-              Отправить
-            </v-btn>
-            <v-btn class="mx-1" @click="formCancl()"> Отмена </v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
-    </v-row>
+            </div>
+          </v-toolbar>
+          <v-divider></v-divider>
+          <v-dialog v-model="dialogParam" max-width="500px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">Новый параметр</span>
+              </v-card-title>
+              <v-divider />
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="Наименование"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.amount"
+                        label="Количество шт."
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.dv"
+                        label="ДВ"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.phase"
+                        label="Фаза внесения"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-text-field
+                        v-model="editedItem.price"
+                        label="Расход и стоимость л/кг на га"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Отмена
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save">
+                  Сохранить
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="650px">
+            <v-card>
+              <v-card-title class="headline"
+                >Вы действитель хотите удалить данный параметр?</v-card-title
+              >
+              <v-divider />
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Нет</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >Да</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-data-table
+            :headers="headers"
+            :items="params"
+            class="elevation-1"
+            hide-default-footer
+          >
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            </template></v-data-table
+          >
+          <v-card-actions class="py-4">
+            <div class="mx-auto">
+              <v-btn
+                class="mx-1"
+                :loading="btnLoader"
+                :disabled="btnStatus"
+                color="green lighten-2 white--text"
+                @click="formSend()"
+              >
+                Отправить
+              </v-btn>
+              <v-btn class="mx-1" @click="formCancl()"> Отмена </v-btn>
+            </div>
+          </v-card-actions>
+        </v-card>
+      </v-row>
     </v-card>
   </v-container>
 </template>
@@ -142,7 +146,7 @@ export default {
     RqCardTitle,
     DialogAfterSendFrom,
     Input,
-    TitleService
+    TitleService,
   },
   data: () => ({
     sub_message:
@@ -170,7 +174,16 @@ export default {
         cs: "12",
         sm: "6",
         md: "6",
-        select_arr: ['СЗР', 'биологические СЗР', 'минеральные удобрения', 'стимуляторы', 'регуляторы роста растений', 'семена', 'инновационные разработки'],
+        select_arr: [
+          "Агротехнологии",
+          "СЗР",
+          "Биологические СЗР",
+          "Минеральные удобрения",
+          "Стимуляторы",
+          "Регуляторы роста растений",
+          "Семена",
+          "Инновационные разработки",
+        ],
         type: "select",
         outlined: true,
         dense: true,
@@ -297,7 +310,7 @@ export default {
       dv: "",
       phase: "",
       price: "",
-    },    
+    },
     file: [],
     params: [],
     disableBtn: false,
@@ -387,7 +400,6 @@ export default {
                 "Успешно. Номер вашей заявки: " + response.data;
               this.btnLoader = false;
               console.log(response.data);
-
             }
           })
           .catch((error) => {
