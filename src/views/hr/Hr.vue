1be3f71c1272db561a8d544583c7efcd6834b5cb
Zaikin Nikolay <kolebas1986@gmail.com>
2022-02-17 09:23:14 +0300

    feat: перевести логику на использование массива

--- src/views/hr/Hr.vue
+++ src/views/hr/Hr.vue
@@ -1,39 +1,57 @@
 <template>
-    <v-container fluid>
-        <br />
+  <v-container fluid>
+    <br />
+    <v-card class="py-12">
+      <div v-for="item in tests" :key="item.route" >
         <v-hover>
-            <template v-slot="{ hover }">
-                <v-card
-                    max-width="400"
-                    @click="$router.push('/test1')"
-                    :elevation="hover ? 24 : 6"
-                >
-                    <v-list-item two-line>
-                        <v-list-item-content>
-                            <v-list-item-title class="headline mb-1"
-                                >Тестирование</v-list-item-title
-                            >
-                            <v-list-item-subtitle
-                                >Тестовые вопросы к инструкциям по уборке
-                                нишевых культур</v-list-item-subtitle
-                            >
-                        </v-list-item-content>
+          <template>
+            <v-card
+              max-width="400"
+              @click="$router.push('/test1')"
+              :elevation="hover ? 24 : 6"
+            >
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{
+                    item.title
+                  }}</v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    item.subtitle
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
 
-                        <v-list-item-avatar size="60" color="grey">
-                            <img src="@/assets/img/hr/tests/test.png" />
-                        </v-list-item-avatar>
-                    </v-list-item>
-                </v-card>
-            </template>
+                <v-list-item-avatar size="60" color="grey">
+                  <img :src="require('../../assets/img/hr/tests/' + item.img)" />
+                </v-list-item-avatar>
+              </v-list-item>
+            </v-card>
+          </template>
         </v-hover>
-    </v-container>
+      </div>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        title: "HR",
-        market_title: "Магазин",
-    }),
+  data: () => ({
+    title: "HR",
+    tests: [
+      {
+        title: "Тест №1",
+        subtitle: "Тестовые вопросы к инструкциям по уборке нишевых культур",
+        route: "/test1",
+        img: "test.png",
+      },
+      {
+        title: "Анкета Увольняющегося",
+        subtitle: "Тестовые вопросы к инструкциям по уборке нишевых культур",
+        route: "/test2",
+        img: "test.png",
+      },
+    ],
+  }),
 };
 </script>
+
+
