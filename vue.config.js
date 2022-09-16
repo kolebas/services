cc5bcd663600a806955aac87f126b62b38688b2e
Николай <kolebas1986@gmail.com>
2022-09-16 08:57:49 +0300

    fix: исключить папку ajax из сборки

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-module.exports = {
+module.exports = { 
   publicPath: process.env.NODE_ENV === 'production',
   "transpileDependencies": [
     "vuetify",
 	"babel"
   ],
+  chainWebpack: (config) => {
+    config.plugin("copy").tap(([options]) => {
+      options[0].ignore.push("ajax/**");
+      return [options];
+    });
+  },
 	filenameHashing: true,
+	// eslint-disable-next-line no-dupe-keys
 	publicPath: '/ahstep/services/',
-  indexPath: 'app.php'
+  indexPath: 'app.php',
 }
